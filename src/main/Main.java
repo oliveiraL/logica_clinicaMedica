cbbc7d533ed8b73e19ee8effe709d8937d395b92
Lucio Oliveira <oliveira.soares.l@hotmail.com>
2016-12-08 20:22:17 -0300

    merge

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,17 +7,12 @@ import clinicaEstetica.dominio.PacienteEstetica;
 import controller.GerenciarPacienteController;
 import dao.PacienteDao;
 import dominio.Paciente;
-<<<<<<< HEAD
-import dominio.Responsavel;
-=======
-import dominio.TipoAtendimento;
->>>>>>> 07cc0e6cb3763bd4816cb87f7918bda6e9d47f5a
 import validacoes.ValidacaoException;
 
 public class Main {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 		GerenciarPacienteController controller = new GerenciarPacienteController();
 		PacienteDao dao = new PacienteDao();
 		// TODO Auto-generated method stub
@@ -46,15 +41,6 @@ public class Main {
 			System.out.println(controller.verificarExistencia(p));
 			System.out.println(controller.buscarPaciente(1));
 			
-=======
-		Especialidade especialidade = new Especialidade("descricao", "designacao");
-		DoctorEstetica doctor = new DoctorEstetica("crm", especialidade, "teste", "CPF", 15);
-		TipoAtendimento tipoAtendimento = new TipoAtendimento("Tipo de Atendimento");
-		try {
-			especialidade.validarEspecialidade();
-			doctor.validarEspecialista();
-			tipoAtendimento.validarTipoAtendimento();
->>>>>>> 07cc0e6cb3763bd4816cb87f7918bda6e9d47f5a
 		} catch (ValidacaoException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
